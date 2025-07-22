0a2059197dbe1fc6ef27b94a27850ca6ed4aaa96
parkdonghyun1993 <cjmon888@gmail.com>
2025-07-22 18:21:47 +0900

    Update server.py

--- server_project/server.py
+++ server_project/server.py
@@ -143,7 +143,7 @@ def login_user():
     now = datetime.now(KST)
     expire = user.get("access_expire")
     if expire:
-    if expire.tzinfo is None:
+     if expire.tzinfo is None:
         expire = KST.localize(expire)
     if now > expire:
         return jsonify({"message": "사용 기간이 만료되었습니다.", "access_granted": False}), 403
